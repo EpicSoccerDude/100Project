2fa2201011ac216af1833aeb6a4ca272ec8bd6ad
EpicSoccerDude <epicdudepj12@gmail.com>
2016-03-06 11:22:48 -0800

    Update animation.js

--- sites/javascript/animation.js
+++ sites/javascript/animation.js
@@ -4,8 +4,8 @@ var h = window.innerHeight;
 var w = window.innerWidth;
 c.height = window.innerHeight;
 c.width = window.innerWidth;
-var x = 25;
-var y = 25;
+var x = 1;
+var y = 1;
 var draw = function() {
 ctx.fillStyle = "violet";
 ctx.fillRect(x,y,150,75);
