477c32408dbeaf18ef1440b11fe8f747c664d3db
EpicSoccerDude <epicdudepj12@gmail.com>
2016-03-16 21:59:04 -0700

    Create dissappear.js

---
+++ sites/javascript/dissappear.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $(".p1").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $(".btn2").click(function(){
+        $(".p2").toggle();
+    });
+});
