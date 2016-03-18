abc102bdc68d8f658eea81431028db1350a1937d
EpicSoccerDude <epicdudepj12@gmail.com>
2016-03-17 20:00:59 -0700

    Update fade.js

--- sites/javascript/fade.js
+++ sites/javascript/fade.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
     $("button").click(function(){
         $("#first").fadeToggle();
-        $("#second").fadeToggle("slow");
-        $("#third").fadeToggle(2000);
     });
 });
